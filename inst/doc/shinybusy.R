84ce825f9e52dd24f709eb9c16c6a8f166978763
Victor Perrier <victor.perrier@dreamrs.fr>
2023-11-23 11:30:02 +0000

    version 0.3.2

--- inst/doc/shinybusy.R
+++ inst/doc/shinybusy.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -7,30 +7,30 @@ knitr::opts_chunk$set(
 ## ----setup--------------------------------------------------------------------
 library(shinybusy)
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  # Add in UI
 #  add_busy_spinner(spin = "fading-circle")
 
-## ---- echo=FALSE--------------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 knitr::include_graphics(path = "figures/add_busy_spinner.png")
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  add_busy_bar(color = "red", height = "8px")
 
-## ---- echo=FALSE--------------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 knitr::include_graphics(path = "figures/add_busy_bar.png")
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  # Add in UI
 #  add_busy_gif(
 #    src = "https://jeroen.github.io/images/banana.gif",
 #    height = 70, width = 70
 #  )
 
-## ---- echo=FALSE--------------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 knitr::include_graphics(path = "figures/add_busy_gif.png")
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  # in UI
 #  use_busy_spinner(spin = "fading-circle")
 #  
@@ -38,14 +38,14 @@ knitr::include_graphics(path = "figures/add_busy_gif.png")
 #  show_spinner() # show the spinner
 #  hide_spinner() # hide the spinner
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  # in UI
 #  use_busy_bar(color = "#01DF01", height = "15px")
 #  
 #  # in server
 #  update_busy_bar(0) # update with the desire value [0-100], 100 hide the bar
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  # in UI
 #  use_busy_gif(
 #    src = "https://jeroen.github.io/images/banana.gif",
@@ -56,21 +56,21 @@ knitr::include_graphics(path = "figures/add_busy_gif.png")
 #  play_gif() # play animation
 #  stop_gif() # stop animation
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  # in server
 #  show_modal_spinner() # show the modal window
 #  remove_modal_spinner() # remove it when done
 
-## ---- echo=FALSE--------------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 knitr::include_graphics(path = "figures/modal_spinner.png")
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  # in server
 #  show_modal_progress_line() # show the modal window
 #  update_modal_progress(0.2) # update progress bar value
 #  remove_modal_progress() # remove it when done
 
-## ---- echo=FALSE--------------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 knitr::include_graphics(path = "figures/modal_progress.png")
 
 ## ----notifications, eval=FALSE------------------------------------------------
